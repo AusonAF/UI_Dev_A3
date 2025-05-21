4f1c6a895ba56767c6f9be246f62176381a6b572
Auson Tseng <aaftseng@hotmail.com>
2025-05-21 23:57:05 +1000

    Add project files.

---
+++ functions.js
@@ -0,0 +1,47 @@
+//
+//							  ______
+//			   				 /      \
+//							/	     \
+//						   /	      \
+//						  /     /\     \
+//						 /_____/  \_____\
+//				  ______________________________
+//				 |					            |
+//               |____________      ____________|
+//                   ______   |    |   ______
+//                  /	  /   |	   |   \     \
+//				   /	 /	  |	   |	\     \
+//				  /	    /	  |	   |     \     \
+//				 /	   /	  |____|      \     \
+//				/     /__________________  \     \
+//			   /					     \  \     \
+//			  /___________________________\  \_____\
+//			       			   ____
+//							  |____|
+//
+//						   AUSON TSENG
+//						 TSENG SHAO HUAN
+//							  -2025-
+
+/*
+
+JavaScript code for the assignment 3 website implementation.
+
+Many thanks to the subject teaching staff including my tutor
+who have supported me throughout this semester.
+
+*/
+
+function openMobileSideBar()
+{
+	var sideBar = document.getElementById("sideBarMobile");
+	
+	sideBarMobile.style.left = "0px";
+}
+
+function closeMobileSideBar()
+{
+	var sideBar = document.getElementById("sideBarMobile");
+	
+	sideBarMobile.style.left = "-400px";
+}
