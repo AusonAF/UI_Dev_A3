d90303dfc931f7cce74cd29e2077f1af734cb0aa
Auson Tseng <aaftseng@hotmail.com>
2025-06-03 19:52:34 +1000

    fully added cart functionality

--- functions.js
+++ functions.js
@@ -64,21 +64,42 @@ function scrollContent(scrollAmount)
 }
 function updateCartItems()
 {
+	let prices = [124.99, 72.99, 49.99, 89.99];
+	let cartTotalPrice = 0;
+	let totalItemsInCart = 0;
 	for (let i = 1; i < 5; i++)
 	{
 		if (localStorage.getItem("product" + i) != "true")
 		{
-			document.getElementById("product" + i.toString()).style.visibility = "collapse";
+			document.getElementById("product" + i.toString()).style.display = "none";
 		}
 		else
 		{
-			console.log("test");
+			document.getElementById("product" + i.toString()).style.display = "inline";
+			cartTotalPrice += parseFloat(prices[i - 1]);
+			totalItemsInCart++;
 		}
 	}
+	cartTotalPrice = Math.round(cartTotalPrice * 100);
+	cartTotalPrice /= 100;
+	document.getElementById("totalPrice").textContent = "Total price: $" + cartTotalPrice.toString();
+	document.getElementById("itemTotal").textContent = totalItemsInCart.toString() + " item(s)";
+	document.getElementById("numberOfItemsInCart").textContent = totalItemsInCart.toString();
 }
 function addToCart(productID)
 {
 	localStorage.setItem("product" + productID.toString(), "true");
+	document.getElementById("productCartButton" + productID.toString()).src = "Images/tickGreen.svg";
+	updateCartItems();
+}
+function addToCartFromProductPage(productID)
+{
+	localStorage.setItem("product" + productID.toString(), "true");
+	document.getElementById("addToCartButton").style.backgroundColor = "green";
+	document.getElementById("addToCartButton").style.backgroundImage = "url(Images/tick.svg)";
+	document.getElementById("addToCartButton").style.boxShadow = "0px 2px 5px #006600";
+	document.getElementById("addToCartButton").textContent = "Added to cart!";
+	updateCartItems();
 }
 function removeFromCart(productID)
 {
